ff48d05da5f1d1edd7b1056607a7828efa86deae
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-10-26 04:40:08 -0700

    Create ::Queue

---
+++ W1D5/queue.rb
@@ -0,0 +1,20 @@
+class Queue
+  attr_reader :queue
+
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    queue.unshift(el)
+  end
+
+  def dequeue
+    queue.pop
+  end
+
+  def peek
+    queue.last
+  end
+
+end
